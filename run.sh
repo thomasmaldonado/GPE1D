27bdf557f825380750d548a92cf36cbb340116e6
Tommy Maldonado <tommymaldonado@dynamic-oit-vapornet100-10-9-105-119.princeton.edu>
2022-06-29 17:07:25 -0400

    undid plotting

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ cp gpe_params.py dependencies
 cd dependencies
 python write_gpe_params_file.py
 matlab -nodisplay < groundstate1d.m > /dev/null 2>&1
-python plotter.py
+#python plotter.py
 cd ..
 cp dependencies/phi.csv .
